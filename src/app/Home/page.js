0b2d2ada5626df97862cfa9850c4b226d1d01fbe
Upelagajanayaka <E208695@esoft.academy>
2025-04-21 21:19:34 +0530

    first commit

---
+++ src/app/Home/page.js
@@ -0,0 +1,21 @@
+use("client");
+import { use } from "react";
+import React from "react";
+export default function landing() {
+    return (
+        <div>
+            <h1>I design and build clean websites</h1>
+            <p>Write anything here something about yourself to showcase what actually you doing or targeting etc.</p>
+            <div>
+                <div>
+                    <div>
+
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+
+    );
+}
