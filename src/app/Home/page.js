111ba2ec72b92d8b60af52da2b4dcc1587016793
Upelagajanayaka <E208695@esoft.academy>
2025-04-21 22:04:42 +0530

    tree comment

--- src/app/Home/page.js
+++ src/app/Home/page.js
@@ -1,25 +1,19 @@
-
 "use client";
-import React from "react";
-import { use } from "react";
 import { useState } from "react";
 import { useEffect } from "react";
 import { useRef } from "react";
+
 export default function landing() {
     return (
-        <div>
-            <h1>I design and build clean websites</h1>
-            <p>Write anything here something about yourself to showcase what actually you doing or targeting etc.</p>
-            <div>
-                <div>
-                    <div>
-
-                    </div>
-                </div>
+        <div style={{ textAlign: "center" }}>
+            <h1 style={{ textAlign: "center" }}>I design and build clean websites</h1>
+            <p style={{ textAlign: "center" }}>
+                Write anything here something about yourself to showcase what actually you doing or targeting etc.
+            </p>
+            <div style={{ display: "flex", justifyContent: "space-between", margin: "20px auto", maxWidth: "300px" }}>
+                <button>Hire me</button>
+                <button>Download CV</button>
             </div>
-
         </div>
-
-
     );
 }
